527784e383996139646fcfd64cfba2a677085bf8
Jose Marin <josemarin@Joses-MacBook-Pro.local>
2025-03-31 18:27:09 -0400

    add a theme file

---
+++ src/theme/theme.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#434371",
+    },
+    secondary: {
+      main: "#70EE9C",
+    },
+    background: {
+      default: "#79AEA3",
+      paper: "#484041",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#B5F44A",
+    },
+  },
+});
+
+export default theme;
