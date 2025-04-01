e47ae7b7b7a11f30eb7766227ee83232f31c5021
Jose Marin <josemarin@Joses-MacBook-Pro.local>
2025-03-31 23:28:08 -0400

    add icons component for social platforms

---
+++ src/components/SocialIcons.tsx
@@ -0,0 +1,58 @@
+import { Box } from "@mui/material";
+import GithubIcon from "../assets/icons/github.svg";
+import LinkedinIcon from "../assets/icons/linkedin.svg";
+import GmailIcon from "../assets/icons/gmail.svg";
+
+const socialLinks = [
+  { href: "https://github.com/josemarin4", icon: GithubIcon, alt: "GitHub" },
+  {
+    href: "https://www.linkedin.com/in/josem-marin/",
+    icon: LinkedinIcon,
+    alt: "LinkedIn",
+  },
+  {
+    href: "mailto:josem.marinhernandez@gmail.com",
+    icon: GmailIcon,
+    alt: "Gmail",
+  },
+];
+
+const SocialIcons = () => {
+  return (
+    <Box sx={{ display: "flex", gap: 3.5, mt: 2 }}>
+      {socialLinks.map(({ href, icon, alt }) => (
+        <a href={href} target="_blank" rel="noopener noreferrer" key={alt}>
+          <Box
+            sx={{
+              width: 56,
+              height: 56,
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "background.paper",
+              boxShadow: 1,
+              transition: "transform 0.2s ease-in-out",
+              "&:hover": {
+                transform: "scale(1.1)",
+              },
+            }}
+          >
+            <Box
+              component="img"
+              src={icon}
+              alt={alt}
+              sx={{
+                width: "65%",
+                height: "65%",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+        </a>
+      ))}
+    </Box>
+  );
+};
+
+export default SocialIcons;
