a6b149e52fdf08715371cc76a4710b98ad1e4e1c
Jose Marin <josemarin@Joses-MacBook-Pro.local>
2025-04-01 23:16:42 -0400

    add a project card component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+import { Project } from "./ProjectsList";
+const ProjectCard = (project: Project) => {
+  return <Box></Box>;
+};
+
+export default ProjectCard;
