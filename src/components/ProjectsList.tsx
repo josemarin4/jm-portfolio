b0473ae29047623ab55853b4d44730528ad67da2
Jose Marin <josemarin@Joses-MacBook-Pro.local>
2025-04-01 23:12:51 -0400

    create a project type

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,4 +1,15 @@
 import { Box } from "@mui/material";
+export type Project = {
+  name: string;
+  description: string;
+  url: string;
+  technologies: string[];
+  images: string[];
+};
+
+const projects: Project[] = [
+  { name: "none", description: "", url: "", technologies: [], images: [] },
+];
 const ProjectsList = () => {
   return <Box></Box>;
 };
